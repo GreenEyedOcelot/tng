8e6b1bbdf72788a0c5fcd9a30829c41ebeb475bd
GreenEyedOcelot <hbe123@gmail.com>
2018-11-16 09:40:32 -0500

    Updates

---
+++ precache-manifest.45412c89056747103c68ca61d7de20f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe19868776bcfe7527b2",
+    "url": "/tng/static/js/runtime~main.fe198687.js"
+  },
+  {
+    "revision": "4689a006688b4a0ac962",
+    "url": "/tng/static/js/main.4689a006.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/tng/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "4689a006688b4a0ac962",
+    "url": "/tng/static/css/main.ce62853f.chunk.css"
+  },
+  {
+    "revision": "20b3c8d6bb507fa4c2bcb0772eeed3c9",
+    "url": "/tng/index.html"
+  }
+];
